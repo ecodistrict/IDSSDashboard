165197a0e558b6768481c3fc1fd4261c9bbfd637
andreasrudena <andreas@rudena.local>
2014-07-08 14:18:47 +0200

    added service for managing decision process

---
+++ client/src/common/services/processService.js
@@ -0,0 +1,44 @@
+angular.module('idss-dashboard')
+
+.factory('ProcessService', ['$http', function ($http) {
+
+    var currentProcess = {
+        district: {
+            center: [1000000, 6600000],
+            area: null,
+            geometry: []
+        },
+        title: "test",
+        isModified: false
+    };
+
+    var saveCurrentProcess = function (credentials) {
+        return $http
+            .post('/process', currentProcess)
+            .then(function (res) {
+                var savedProcess = res.data;
+                currentProcess.lastSaved = savedProcess.lastSaved;
+                setIsModified(false);
+                return currentProcess;
+            });
+    };
+
+    var getCurrentProcess = function() {
+        return currentProcess;
+    };
+
+    var getIsModified = function() {
+        return currentProcess.isModified;
+    };
+
+    var setIsModified = function(isModified) {
+        currentProcess.isModified = isModified;
+    };
+
+    return {
+        saveCurrentProcess: saveCurrentProcess,
+        getCurrentProcess: getCurrentProcess,
+        getIsModified: getIsModified,
+        setIsModified: setIsModified
+    };
+}]);
