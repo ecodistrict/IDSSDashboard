7906c0883b22c2e65918a603a6276eea04f27343
andreasrudena <andreas.rudena@strusoft.com>
2014-09-12 14:20:28 +0200

    add icons for next button, change module list in collect-data

--- client/src/app/02-collect-data/collect-data.js
+++ client/src/app/02-collect-data/collect-data.js
@@ -30,29 +30,12 @@ angular.module( 'idss-dashboard.collect-data', [
   console.log($scope.currentProcess);
   _.each($scope.currentProcess.kpiList, function(kpi) {
     if(kpi.selectedModule) {
-      $scope.moduleList.push({kpi: kpi.name, module: kpi.selectedModule.name});
+      $scope.moduleList.push({
+        kpi: kpi, 
+        module: kpi.selectedModule
+      });
     }
   });
-
-  $scope.configureKpi = function(kpi) {
-
-    var kpiModal = $modal.open({
-      templateUrl: '02-collect-data/module-indata.tpl.html',
-      controller: 'ModuleIndataCtrl',
-      resolve: {
-        kpi: function() {
-          return kpi;
-        }
-      }
-    });
-
-    kpiModal.result.then(function (configuredKpi) {
-      console.log(configuredKpi);
-    }, function () {
-      console.log('Modal dismissed at: ' + new Date());
-    });
-
-  };
 
   $scope.moduleIndataIsOk = function(module) {
     return false;
