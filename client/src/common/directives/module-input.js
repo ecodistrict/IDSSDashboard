8cf55dd71db865655050726a47523c8576eb35a8
andreasrudena <andreas.rudena@strusoft.com>
2014-09-15 14:07:45 +0200

    module input directives added

--- client/src/common/directives/module-input.js
+++ client/src/common/directives/module-input.js
@@ -2,17 +2,28 @@ angular.module('idss-dashboard').directive('moduleInput', ['$compile', function(
 
     return {
         restrict: 'E',
+        scope: {
+            moduleInputs: '='
+        },
         link: function ( scope, element, attrs ) {
 
-            var inputs = attrs.inputs;
+            console.log(scope.moduleInputs);
 
-            console.log(inputs.inputs);
-            
+            // set template links
+            _.each(scope.moduleInputs, function(input) {
+                input.template = 'directives/module-inputs/' + input.type + '.tpl.html';
+            });
+
+            // TODO: How to dynamically add directives depending on type
+            // TODO: create input-group recursively
             var template =
-            '<div ng-repeat="input in ' + inputs + '">' +
-                '{{input.type}}' + 
-            '</div>' +
-            (inputs.inputs ? '<module-input inputs="' + inputs.inputs + '">' : '');
+            '<div ng-repeat="input in moduleInputs" ng-include="input.template">' + 
+                // '{{input.type}}' + 
+                // '<module-input-text ng-if="input.type == \'text\'" input="input">' + 
+                // '<module-input-number ng-if="input.type == \'number\'" input="input">' + 
+                // '<module-input-input-group ng-if="input.type == \'input-group\'" input="input">' + 
+                //'<module-input module-inputs="{{input.inputs}}>' +  
+            '</div>';
 
             element.html('').append( $compile( template )( scope ) );
         }
