6be2876e9b6bc11d997da63f696987cd37753064
andreasrudena <andreas.rudena@strusoft.com>
2014-10-24 15:07:41 +0200

    functionality for adding kpis

---
+++ client/src/app/01-analyse-problem/add-kpi.js
@@ -0,0 +1,34 @@
+angular.module( 'idss-dashboard.analyse-problem.add-kpi', [])
+
+.controller( 'AddKpiCtrl', ['$scope', '$modalInstance', function AddKpiCtrl( $scope, $modalInstance) {
+
+  	$scope.kpi = {
+      official: false,
+      inputs: []
+    };
+    $scope.qualitative = false;
+
+    var generateInputs = function() {
+      var k = $scope.kpi;
+      if(k.unit && k.min && k.max) {
+        k.inputs.push({
+          label: 'Select a value between ' + k.min + ' ' + k.unit + ' and ' + k.max + ' ' + k.unit,
+          type: "number",
+          min: k.min,
+          max: k.max,
+          unit: k.unit
+        });
+      }
+      // continue adding more controls from kpi settings - quantitative fields and so on
+    };
+
+    $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+    };
+
+  	$scope.ok = function() {
+      generateInputs();
+      $modalInstance.close($scope.kpi);
+  	};
+
+}]);
