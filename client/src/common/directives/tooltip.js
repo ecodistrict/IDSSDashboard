4c506abf7f1f899b007ea57766854a9a76a4b07c
andreasrudena <andreas.rudena@strusoft.com>
2014-10-25 18:07:58 +0200

    directive for tooltip

---
+++ client/src/common/directives/tooltip.js
@@ -0,0 +1,12 @@
+angular.module('idss-dashboard').directive('tooltip', function(){
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs){
+            $(element).hover(function(){
+                $(element).tooltip('show');
+            }, function(){
+                $(element).tooltip('hide');
+            });
+        }
+    };
+});
