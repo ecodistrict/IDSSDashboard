8ca0ea43d50283f9b56acfd3c0e67b2c557416ed
andreasrudena <andreas.rudena@strusoft.com>
2014-10-27 16:30:52 +0100

    remove current process

--- client/src/app/header/header.js
+++ client/src/app/header/header.js
@@ -1,6 +1,6 @@
 angular.module( 'idss-dashboard.header', [])
 
-.controller( 'HeaderCtrl', ['$scope', '$location', 'LoginService', '$state', '$rootScope', function HeaderCtrl( $scope, $location, LoginService, $state, $rootScope ) {
+.controller( 'HeaderCtrl', ['$scope', '$location', 'LoginService', '$state', '$rootScope', 'ProcessService', function HeaderCtrl( $scope, $location, LoginService, $state, $rootScope, ProcessService ) {
 
     $scope.logout = function() {
 
@@ -15,5 +15,14 @@ angular.module( 'idss-dashboard.header', [])
         $scope.highLightInMenu = urlParts[0] === "" ? urlParts[1] : urlParts[0];
 
     });
+
+    $scope.deleteProcess = function() {
+    	ProcessService.deleteCurrentProcess().then(function() {
+    		// when loading a non existing process, a new process is created
+    		ProcessService.loadCurrentProcess().then(function(newProcess) {
+    			$scope.currentProcess = newProcess;
+    		});
+    	});
+    };
 
 }]);
