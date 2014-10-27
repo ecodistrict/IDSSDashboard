ae92cb78200f631fdc1765638bb09b3ea4f677d7
andreasrudena <andreas.rudena@strusoft.com>
2014-10-27 15:49:52 +0100

    generator of all inputs, rename later

--- client/src/common/directives/kpi-input.js
+++ client/src/common/directives/kpi-input.js
@@ -1,5 +1,8 @@
 angular.module('idss-dashboard').directive('kpiInput', ['$compile', function($compile) {
 
+    // a simple check to see if input is not misspelled (that will crash browser because of ng-include cant find template)
+    var registeredInputs = ['number', 'input-group'];
+
     return {
         restrict: 'E',
         scope: {
@@ -13,19 +16,17 @@ angular.module('idss-dashboard').directive('kpiInput', ['$compile', function($co
             var setTemplateUrl = function(inputs) {
                 inputs = inputs || [];
                 _.each(inputs, function(input) {
-                  input.template = 'directives/inputs/' + input.type + '.tpl.html';
-                  // skip recursion here, do that in respective child
-                  // if(input.inputs) {
-                  //   setTemplateUrl(input.inputs);
-                  // }
+                    if(_.find(registeredInputs, function(rI) {return rI === input.type;})) {
+                        input.template = 'directives/inputs/' + input.type + '.tpl.html';
+                    } else {
+                        input.template = 'directives/inputs/not-found.tpl.html';
+                    }
                 });
             };
 
             setTemplateUrl(scope.inputs);
 
             var template = '<div ng-repeat="input in inputs" ng-include="input.template"></div>';
-            // skip recursion here, do that in respective child
-            //(scope.inputs.inputs ? '<kpi-input inputs="inputs">' : '');
 
             element.html('').append( $compile( template )( scope ) );
 
