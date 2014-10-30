91144164a1d49bae8593dd40d5e8bfc552d3898b
andreasrudena <andreas.rudena@strusoft.com>
2014-10-30 07:07:15 +0100

    kpi input watch for loading the input on page load

--- client/src/common/directives/kpi-input.js
+++ client/src/common/directives/kpi-input.js
@@ -1,7 +1,7 @@
 angular.module('idss-dashboard').directive('kpiInput', ['$compile', function($compile) {
 
     // a simple check to see if input is not misspelled (that will crash browser because of ng-include cant find template)
-    var registeredInputs = ['number', 'input-group', 'slider'];
+    var registeredInputs = ['number', 'input-group', 'slider', 'geojson'];
 
     return {
         restrict: 'E',
@@ -10,26 +10,35 @@ angular.module('idss-dashboard').directive('kpiInput', ['$compile', function($co
         },
         link: function ( scope, element, attrs ) {
 
-            console.log(scope.inputs);
+            var render = function() {
 
-            // set template urls to all inputs to generate corresponding directive
-            var setTemplateUrl = function(inputs) {
-                inputs = inputs || [];
-                _.each(inputs, function(input) {
-                    if(_.find(registeredInputs, function(rI) {return rI === input.type;})) {
-                        input.template = 'directives/inputs/' + input.type + '.tpl.html';
-                    } else {
-                        input.template = 'directives/inputs/not-found.tpl.html';
-                    }
-                });
+                // set template urls to all inputs to generate corresponding directive
+                var setTemplateUrl = function(inputs) {
+                    inputs = inputs || [];
+                    _.each(inputs, function(input) {
+                        if(_.find(registeredInputs, function(rI) {return rI === input.type;})) {
+                            input.template = 'directives/inputs/' + input.type + '.tpl.html';
+                        } else {
+                            input.template = 'directives/inputs/not-found.tpl.html';
+                        }
+                    });
+                };
+
+                setTemplateUrl(scope.inputs);
+
+                var template = '<div ng-repeat="input in inputs" ng-include="input.template"></div>';
+
+                element.html('').append( $compile( template )( scope ) );
+
             };
 
-            setTemplateUrl(scope.inputs);
-
-            var template = '<div ng-repeat="input in inputs" ng-include="input.template"></div>';
-
-            element.html('').append( $compile( template )( scope ) );
-
+            scope.$watchCollection('inputs', function(newInputs, oldInputs) {
+                console.log(newInputs, oldInputs);
+                if(newInputs && newInputs.length) {
+                    console.log(newInputs);
+                    render();
+                }
+            });
         }
     };
 
