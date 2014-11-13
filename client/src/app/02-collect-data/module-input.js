eda1bd16296f2d18f52c2b9c297652345c158acf
andreasrudena <andreas.rudena@strusoft.com>
2014-11-13 19:56:09 +0100

    use kpiAlias

--- client/src/app/02-collect-data/module-input.js
+++ client/src/app/02-collect-data/module-input.js
@@ -25,12 +25,20 @@ angular.module( 'idss-dashboard.collect-data.module-input', [])
         } else {
           return VariantService.loadVariants();
         }
+      }],
+      currentProcess: ['ProcessService', function(ProcessService) {
+        var p = ProcessService.getCurrentProcess();
+        if(p) {
+          return p;
+        } else {
+          return ProcessService.loadCurrentProcess();
+        }
       }]
     } 
   });
 }])
 
-.controller( 'ModuleInputCtrl', ['$scope', '$stateParams', 'variants', 'ModuleService', function ModuleInputCtrl( $scope, $stateParams, variants, ModuleService ) {
+.controller( 'ModuleInputCtrl', ['$scope', '$stateParams', 'variants', 'ModuleService', 'currentProcess', function ModuleInputCtrl( $scope, $stateParams, variants, ModuleService, currentProcess ) {
 
   if(!$stateParams.kpiAlias) {
     console.log('missing params');
@@ -38,6 +46,7 @@ angular.module( 'idss-dashboard.collect-data.module-input', [])
   }
 
   $scope.asIsVariant = _.find(variants, function(v) {return v.type === 'as-is';});
+  $scope.currentProcess = currentProcess;
 
   var kpi = _.find($scope.asIsVariant.kpiList, function(k) {
     return k.alias === $stateParams.kpiAlias;
@@ -54,7 +63,7 @@ angular.module( 'idss-dashboard.collect-data.module-input', [])
     ModuleService.saveModuleInput($scope.asIsVariant._id, $scope.module);
   };
 
-  ModuleService.getModuleInput($scope.asIsVariant._id, kpi.selectedModule.id).then(function(module) {
+  ModuleService.getModuleInput($scope.asIsVariant._id, kpi.selectedModule.id, kpi.alias).then(function(module) {
     // TODO: fix a way to update save button
     // module.isModified = false; // init a save input flag
     module.isModified = true;
