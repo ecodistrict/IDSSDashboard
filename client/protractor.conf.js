4d44ed13b32404ff6a522e793ec4dfb951f20397
andreasrudena <andreas.rudena@strusoft.com>
2015-04-18 10:04:30 +0200

    restructure test to start with atomic input test

--- client/protractor.conf.js
+++ client/protractor.conf.js
@@ -1,21 +1,23 @@
 exports.config = {
     specs: [
+        //basic setup
     	'test/e2e/create-new-account.e2e.js',
     	'test/e2e/logging-in.e2e.js',
         'test/e2e/check-modules-registered.e2e.js',
         'test/e2e/analyse-problem-overview.e2e.js',
-        'test/e2e/add-kpi-geojson.e2e.js',
-        'test/e2e/use-kpi-geojson.e2e.js',
-        'test/e2e/configure-kpi-geojson.e2e.js',
-        //'test/e2e/collect-data-geojson.e2e.js',
-        //'test/e2e/calculate-geojson'
+        //quantitative KPI
         'test/e2e/add-kpi-atomic.e2e.js',
         'test/e2e/use-kpi-atomic.e2e.js',
-        'test/e2e/configure-kpi-atomic.e2e.js',
-        'test/e2e/collect-data-atomic.e2e.js'
+        'test/e2e/configure-kpi-atomic.e2e.js'
+        // 'test/e2e/add-kpi-geojson.e2e.js',
+        // 'test/e2e/use-kpi-geojson.e2e.js',
+        // 'test/e2e/configure-kpi-geojson.e2e.js',
+        //'test/e2e/collect-data-geojson.e2e.js',
+        //'test/e2e/calculate-geojson'
+        //'test/e2e/collect-data-atomic.e2e.js'
         //'test/e2e/calculate-atomic'
     ],
     baseUrl: 'http://localhost:3300',
-    seleniumServerJar: 'node_modules/protractor/selenium/selenium-server-standalone-2.44.0.jar',
-    chromeDriver: 'node_modules/protractor/selenium/chromedriver'
+    seleniumServerJar: '/usr/local/lib/node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar',
+    chromeDriver: '/usr/local/lib/node_modules/protractor/selenium/chromedriver'
 }
