3e1dfe7577bb96052c9037b659d8ce69ddf8dae1
andreasrudena <andreas.rudena@strusoft.com>
2015-06-10 09:49:44 +0200

    add more input types

--- test/modules/testmodule.js
+++ test/modules/testmodule.js
@@ -25,6 +25,65 @@ var moduleDefinition = {
   kpiList: [kpi]
 };
 
+var geojsonInputSpecification = {
+  "type": "geojson",
+  "label": "Define buildings with GeoJSON polygon geometries",
+  "inputs": {
+    "num-storeys": {
+      "type": "number",
+      "label": "Number of storeys",
+      "min": 1,
+      "max": 10,
+      "value": 3
+    },
+    "glazing": {
+        "type": "number",
+        "label": "Glazing area",
+        "unit": "%",
+        "min": 1,
+        "max": 100,
+        "value": 40
+    }, 
+    "year-of-construction": {
+        "type": "number",
+        "label": "Year of construction",
+        "min": 1400,
+        "max": 2100,
+        "value": 1930
+    }, 
+    "usage": {
+        "type": "select",
+        "options": [{
+          "value": "detached",
+          "label": "Detached house"
+        }, {
+          "value": "block-of-flats",
+          "label": "Block of flats"
+        }, {
+          "value": "office",
+          "label": "Office"
+        }],
+        "label": "Usage",
+        "value": "block-of-flats"
+    }, 
+    "building-type": {
+        "type": "select",
+        "options": [{
+          "value": "concrete",
+          "label": "Concrete"
+        }, {
+          "value": "wood",
+          "label": "Wooden"
+        }, {
+          "value": "brick",
+          "label": "Brick"
+        }],
+        "label": "Building type",
+        "value": "wood"
+    }
+  }
+};
+
 // selectModule response
 var moduleInput = {
     "method": "selectModule",
@@ -32,63 +91,47 @@ var moduleInput = {
     "moduleId": moduleId,
     "kpiId": kpi,
     "inputSpecification": {
-      "buildings": {
-        "type": "geojson",
-        "geometryObject": "polygon",
-        "label": "Define buildings with GeoJSON polygon geometries",
+      "buildings": geojsonInputSpecification,
+      "district": {
+        "type": "district-polygon",
+        "projection": "EPSG:3857"
+      }, 
+      "name-1": { "type": "text", "label": "Name", "order": -1 },
+      "include": { "type": "checkbox", "label": "Include", "order": 2 },
+      "shoe-size-1": { "type": "number", "label": "Shoe size" },
+      "shoe-brand": { "type": "text", "label": "Shoe brand" },
+      "cheese-type": {
+        "label": "Cheese type",
+        "type": "select",
+        "value": "brie-cheese",
+        "options": [
+          {
+            "value": "alp-cheese",
+            "label": "Alpk\u00e4se"
+          },
+          {
+            "value": "edam-cheese",
+            "label": "Edammer"
+          },
+          {
+            "value": "brie-cheese",
+            "label": "Brie"
+          }
+        ]
+      },
+      "personal-data": {
+        "type": "inputGroup",
+        "label": "Personal data",
         "inputs": {
-            "num-storeys": {
-              "type": "number",
-              "label": "Number of storeys",
-              "min": 1,
-              "max": 10,
-              "value": 3
-            },
-            "glazing": {
-                "type": "number",
-                "label": "Glazing area",
-                "unit": "%",
-                "min": 1,
-                "max": 100,
-                "value": 40
-            }, 
-            "year-of-construction": {
-                "type": "number",
-                "label": "Year of construction",
-                "min": 1400,
-                "max": 2100,
-                "value": 1930
-            }, 
-            "usage": {
-                "type": "select",
-                "options": [{
-                  "value": "detached",
-                  "label": "Detached house"
-                }, {
-                  "value": "block-of-flats",
-                  "label": "Block of flats"
-                }, {
-                  "value": "office",
-                  "label": "Office"
-                }],
-                "label": "Usage",
-                "value": "block-of-flats"
-            }, 
-            "building-type": {
-                "type": "select",
-                "options": [{
-                  "value": "concrete",
-                  "label": "Concrete"
-                }, {
-                  "value": "wood",
-                  "label": "Wooden"
-                }, {
-                  "value": "brick",
-                  "label": "Brick"
-                }],
-                "label": "Building type",
-                "value": "wood"
-            }
+          "name-2": {
+            "type": "text",
+            "label": "Your name"
+          },
+          "shoe-size-2": {
+            "value": 42,
+            "type": "number",
+            "label": "Your shoe size"
+          }
         }
       }
     }
@@ -159,7 +202,7 @@ frameworkSub.onString = function(aEventEntry, aString) {
 
       moduleResult.outputs = [{
         type: "kpi",
-        value: 8,
+        value: 800,
         info: "Mean value.."
       },{
         type: "geojson",
