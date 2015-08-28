45f1c0c32c97a5df3358055809592c27c5feb1aa
andreasrudena <andreas.rudena@strusoft.com>
2015-08-28 11:52:38 +0200

    move button, add weight and minimum at correct place

--- client/src/app/07-compare-variants/compare-variants.js
+++ client/src/app/07-compare-variants/compare-variants.js
@@ -62,8 +62,15 @@ angular.module( 'idss-dashboard.compare-variants', [])
 
         // map on variant and kpi
         kpiValueMap[record.userId] = kpiValueMap[record.userId] || {};
+
         kpiValueMap[record.userId][record.variantId] = kpiValueMap[record.userId][record.variantId] || {};
-        kpiValueMap[record.userId][record.variantId][record.kpiAlias] = {value: record.value, disabled: record.disabled};
+        // this map is for weights and minimum (one for every user and kpi)
+        kpiValueMap[record.userId][record.kpiAlias] = kpiValueMap[record.userId][record.kpiAlias] || {};
+        // this map is for values on each variant
+        kpiValueMap[record.userId][record.variantId][record.kpiAlias] = {value: record.value};
+        // this map set min, weight
+        kpiValueMap[record.userId][record.kpiAlias] = {weight: record.weight, minimum: record.minimum};
+
       });
 
       // create structure user/variant/kpi
@@ -77,17 +84,23 @@ angular.module( 'idss-dashboard.compare-variants', [])
           variants: [],
           kpiList: []
         };
-        // add kpi to stakeholder TODO: add weight
         _.each(currentProcess.kpiList, function(kpi) {
-          stakeholderData.kpiList.push({
+          var kpiBaseData = {
             kpiName: kpi.name,
             kpiDescription: kpi.description,
             kpiId: kpi.kpiAlias,
-            bad: kpi.bad,
             unit: kpi.unit,
-            weight: kpi.weight,
+            sufficient: kpi.sufficient,
             excellent: kpi.excellent
-          });
+          };
+          if(kpiValueMap[user._id] && kpiValueMap[user._id][kpi.kpiAlias]) {
+            kpiBaseData.weight = kpiValueMap[user._id][kpi.kpiAlias].weight;
+            kpiBaseData.minimum = kpiValueMap[user._id][kpi.kpiAlias].minimum;
+
+            console.log(kpiBaseData);
+          }
+          stakeholderData.kpiList.push(kpiBaseData);
+
         });
         
         // add to global data object
@@ -107,11 +120,10 @@ angular.module( 'idss-dashboard.compare-variants', [])
           // add kpis to kpi list
           _.each(currentProcess.kpiList, function(kpi) {
 
-            var kpiValue, disabled;
+            var kpiValue, weight, minimum;
 
-            if(kpiValueMap[user._id] && kpiValueMap[user._id][variant._id] && (kpiValueMap[user._id][variant._id][kpi.kpiAlias] || kpiValueMap[user._id][variant._id][kpi.kpiAlias] === 0)) {
+            if(kpiValueMap[user._id] && kpiValueMap[user._id][variant._id] && kpiValueMap[user._id][variant._id][kpi.kpiAlias]) {
               kpiValue = kpiValueMap[user._id][variant._id][kpi.kpiAlias].value;
-              disabled = kpiValueMap[user._id][variant._id][kpi.kpiAlias].disabled;
             } else {
               // if this is undefined not even the facilitator has given a value to the kpi (no record has been found for variant)
               if(kpiDefaultValueMap[variant._id]) {
@@ -121,8 +133,7 @@ angular.module( 'idss-dashboard.compare-variants', [])
             // create kpi data
             var kpiData = {
               kpiId: kpi.kpiAlias,
-              kpiValue: kpiValue,
-              disabled: disabled
+              kpiValue: kpiValue
             };
             // add to variant reference
             variantData.kpiList.push(kpiData);
